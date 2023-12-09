d39238118fe81d46cefb8b365e2f78ce3dbd607c
Madhur Gupta <madhurgupta107@gmail.com>
2023-12-09 13:00:29 +0530

    fix explorer UI

--- frontend-history-explorer/src/app/page.js
+++ frontend-history-explorer/src/app/page.js
@@ -1,79 +1,70 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/Ap9KXWZm3oi
- */
+"use client";
+
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Component() {
+  const [data, setData] = useState([]);
+  const fetchData = () => {
+    // Fetch data here and update the state
+    // This is just a placeholder data
+    setData([
+      {
+        id: 1,
+        version: "Version 1",
+        image: "",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      },
+      {
+        id: 2,
+        version: "Version 2",
+        image: "",
+        description:
+          "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      },
+      {
+        id: 3,
+        version: "Version 3",
+        image: "",
+        description:
+          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      },
+    ]);
+  };
+
   return (
     <div className="max-w-4xl mx-auto my-8">
-      <div className="flex items-center mb-8">
+      <div className="flex items-center mb-12">
         <Input className="flex-1 mr-4" placeholder="Enter identifier" />
-        <Button>Search</Button>
+        <Button className="bg-black text-white" onClick={fetchData}>Search</Button>
       </div>
+
       <div>
-        <div className="flex items-center mb-4">
-          <img
-            alt="Version 1"
-            className="block w-24 h-24 mr-4"
-            height="100"
-            src="/placeholder.svg"
-            style={{
-              aspectRatio: "100/100",
-              objectFit: "cover",
-            }}
-            width="100"
-          />
-          <ArrowDownIcon className="text-gray-500" />
-          <div className="ml-4">
-            <p className="font-semibold text-lg mb-1">Version 1</p>
-            <p className="text-gray-600">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
+        {data.map((item, index) => (
+          <div key={item.id}>
+            <div className="flex items-center mb-4">
+              <img
+                alt={item.version}
+                className="block w-24 h-24 mr-4 border border-gray-200 rounded"
+                height="100"
+                src={item.image}
+                style={{
+                  aspectRatio: "100/100",
+                  objectFit: "cover",
+                }}
+                width="100"
+              />
+              <div className="ml-4">
+                <p className="font-semibold text-lg mb-1">{item.version}</p>
+                <p className="text-gray-600">{item.description}</p>
+              </div>
+            </div>
+            {index < data.length - 1 && (
+              <ArrowDownIcon className="text-gray-500 ml-9 mb-5" />
+            )}
           </div>
-        </div>
-        <div className="flex items-center mb-4">
-          <img
-            alt="Version 2"
-            className="block w-24 h-24 mr-4"
-            height="100"
-            src="/placeholder.svg"
-            style={{
-              aspectRatio: "100/100",
-              objectFit: "cover",
-            }}
-            width="100"
-          />
-          <ArrowDownIcon className="text-gray-500" />
-          <div className="ml-4">
-            <p className="font-semibold text-lg mb-1">Version 2</p>
-            <p className="text-gray-600">
-              Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <img
-            alt="Version 3"
-            className="block w-24 h-24 mr-4"
-            height="100"
-            src="/placeholder.svg"
-            style={{
-              aspectRatio: "100/100",
-              objectFit: "cover",
-            }}
-            width="100"
-          />
-          <ArrowDownIcon className="text-gray-500" />
-          <div className="ml-4">
-            <p className="font-semibold text-lg mb-1">Version 3</p>
-            <p className="text-gray-600">
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
