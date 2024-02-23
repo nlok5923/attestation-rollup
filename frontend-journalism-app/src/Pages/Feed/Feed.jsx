8bf8b0eca562e70ebc4fd2ea3a2db6d8ffdc59b7
Madhur Gupta <madhurgupta107@gmail.com>
2024-02-23 20:17:07 +0530

    Fix journalism app feed page

--- frontend-journalism-app/src/Pages/Feed/Feed.jsx
+++ frontend-journalism-app/src/Pages/Feed/Feed.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 import { Card } from "antd";
 import "./Feed.css";
 
@@ -8,59 +8,60 @@ import img2 from "../../data/img-2.jpeg";
 import img3 from "../../data/img-3.jpeg";
 import img4 from "../../data/img-4.jpeg";
 
-const Feed = () => {
-  const [post, setPost] = useState([]);
-  const [content, setContent] = useState([]);
-  const [isLoad, setIsLoad] = useState(false);
-  const [img, setImg] = useState("");
-  const [uuid, setUuid] = useState("");
+const OLD_POSTS = [
+  {
+    title: "Hackers",
+    content:
+      "A look into the world of ethereum and the role it played in the success of ETH India 2023.",
+    image: img1,
+  },
+  {
+    title: "The venue",
+    content:
+      "A detailed overview of the venue that hosted the grand ETH India 2023 hackathon.",
+    image: img2,
+  },
+  {
+    title: "Close to the finish line",
+    content:
+      "The nail-biting final stages of the ETH India 2023 hackathon and the intensity it brought to the participants.",
+    image: img3,
+  },
+  {
+    title: "ETH India",
+    content:
+      "A comprehensive recap of the highlights and key takeaways from the ETH India 2023 hackathon.",
+    image: img4,
+  },
+];
 
-  const _fetchPost = async () => {
-    setIsLoad(true);
-    // const data = await contract.getAllPosts();
-    setPost([
-      {
-        title: "Hackers",
-        content:
-          "A look into the world of ethereum and the role it played in the success of ETH India 2023.",
-        image: img1,
-      },
-      {
-        title: "The venue",
-        content:
-          "A detailed overview of the venue that hosted the grand ETH India 2023 hackathon.",
-        image: img2,
-      },
-      {
-        title: "Close to the finish line",
-        content:
-          "The nail-biting final stages of the ETH India 2023 hackathon and the intensity it brought to the participants.",
-        image: img3,
-      },
-      {
-        title: "ETH India",
-        content:
-          "A comprehensive recap of the highlights and key takeaways from the ETH India 2023 hackathon.",
-        image: img4,
-      },
-    ]); // Placeholder for fetched data
-    setIsLoad(false);
-  };
-  const _fetchData = async () => {
-    const data = await axios.get('https://094c-44-192-50-105.ngrok-free.app/',
-    {
-      headers: {
-        "ngrok-skip-browser-warning": "true" 
-      }
-    });
-    console.log(data.data);
-    setContent(data.data);
-    setImg(data.data.contentState[data.data.contentState.length - 1].updatedContent);
-    setUuid(data.data.contentState[data.data.contentState.length - 1].uuid);
-  }
+const Feed = () => {
+  const [fetchedPost, setFetchedPost] = useState(OLD_POSTS);
 
   useEffect(() => {
-    _fetchPost(); // Fetch data on component mount
+    const _fetchData = async () => {
+      const { data } = await axios.get(
+        "https://094c-44-192-50-105.ngrok-free.app/",
+        {
+          headers: {
+            "ngrok-skip-browser-warning": "true",
+          },
+        }
+      );
+
+      console.log(data.contentState[data.contentState.length - 1]);
+
+      setFetchedPost({
+        title: "Latest Image",
+        content: "From Pixel Police Mobile App",
+        image:
+          "data:image/png;base64," +
+          data.contentState[data.contentState.length - 1].updatedContent,
+        historyLink: `https://pixel-police-history-explorer.vercel.app/${
+          data.contentState[data.contentState.length - 1].uuid
+        }`,
+      });
+    };
     _fetchData();
   }, []);
 
@@ -79,34 +80,8 @@ const Feed = () => {
             Citizen Journalism App
           </h1>
         </div>
-        <Card className="w-full">
-                <img
-                  alt=""
-                  className="w-full h-48 object-cover"
-                  height="200"
-                  src={'data:image/png;base64,' + img}
-                  style={{
-                    aspectRatio: "350/200",
-                    objectFit: "cover",
-                  }}
-                  width="350"
-                />
-                <div>
-                  <div className="flex items-center mt-4 mb-2">
-                    <h2 className="text-lg font-semibold">Latest Image</h2>
-                  </div>
-                  <p className="text-gray-600 text-left">From Pixel Police Mobile App</p>
-                </div>
-                <div className="flex justify-end mt-6">
-                <a href={`https://pixel-police-history-explorer.vercel.app/${uuid}`}>
-                  <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
-                    View History
-                  </button>
-                </a>
-                </div>
-          </Card>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {post.map(
+          {[fetchedPost, ...OLD_POSTS].map(
             (
               item,
               index // Render from post state
@@ -130,9 +105,14 @@ const Feed = () => {
                   <p className="text-gray-600 text-left">{item.content}</p>
                 </div>
                 <div className="flex justify-end mt-6">
-                  <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
+                  <a
+                    href={item.historyLink ?? ""}
+                    target="_blank"
+                    className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
+                    rel="noreferrer"
+                  >
                     View History
-                  </button>
+                  </a>
                 </div>
               </Card>
             )
